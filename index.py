fd5608c13ccecab1aee5a757a558e9f41ade48e6
KTS224 <ts4683@gmail.com>
2023-12-02 18:05:08 +0900

    + error

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask import render_template
-import RPi.GPIO as GPIO
+
 
 import requests
 import json
@@ -158,36 +158,35 @@ def get_windspeed():
 		
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
- 
-led_R = 20
-led_Y = 21
-sensor = 4
-# 불필요한 warning 제거, GPIO핀의 번호 모드 설정
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-# LED의 GPIO 핀(led_R, led_Y)을 출력으로 설정하고 PIR 센서의 GPIO 핀(sensor)을 입력으로 설정
-GPIO.setup(led_R, GPIO.OUT)
-GPIO.setup(led_Y, GPIO.OUT)
-GPIO.setup(sensor, GPIO.IN)
-print("PIR Ready ....")
-time.sleep(5)
+	led_R = 20
+	led_Y = 21
+	sensor = 4
+	# 불필요한 warning 제거, GPIO핀의 번호 모드 설정
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	# LED의 GPIO 핀(led_R, led_Y)을 출력으로 설정하고 PIR 센서의 GPIO 핀(sensor)을 입력으로 설정
+	GPIO.setup(led_R, GPIO.OUT)
+	GPIO.setup(led_Y, GPIO.OUT)
+	GPIO.setup(sensor, GPIO.IN)
+	print("PIR Ready ....")
+	time.sleep(5)
 
-try:
-	while True:
-	# PIR 센서는 사람의 움직임을 감지한 경우 3.3V(high 신호,1)을 출력하므로 PIR 센서의 GPIO 핀(sensor)은 3.3V(high 신호,1)을 입력받음
-		if GPIO.input(sensor) == 1:
-			GPIO.output(led_Y, 1) # 노란색 LED 켬
-			GPIO.output(led_R, 0) # 빨간색 LED 끔
-			print("Motion Detected !")
-			debugNumber = 10
-			time.sleep(2)
-			# PIR 센서는 사람의 움직임을 감지하지 못한 경우 0V(low 신호,0)을 출력하므로 PIR 센서의 GPIO 핀(sensor)은 0V(low 신호,0)을 입력받음
-		if GPIO.input(sensor) == 0:
-			GPIO.output(led_R, 1) # 빨간색 LED 켬
-			GPIO.output(led_Y, 0) # 노란색 LED 끔
-			print("Motion Not Detected !")
-			debugNumber = -443
-			time.sleep(2)
-except KeyboardInterrupt:
-	print("Stopped by User")
-	GPIO.cleanup() 
+	try:
+		while True:
+		# PIR 센서는 사람의 움직임을 감지한 경우 3.3V(high 신호,1)을 출력하므로 PIR 센서의 GPIO 핀(sensor)은 3.3V(high 신호,1)을 입력받음
+			if GPIO.input(sensor) == 1:
+				GPIO.output(led_Y, 1) # 노란색 LED 켬
+				GPIO.output(led_R, 0) # 빨간색 LED 끔
+				print("Motion Detected !")
+				debugNumber = 10
+				time.sleep(2)
+				# PIR 센서는 사람의 움직임을 감지하지 못한 경우 0V(low 신호,0)을 출력하므로 PIR 센서의 GPIO 핀(sensor)은 0V(low 신호,0)을 입력받음
+			if GPIO.input(sensor) == 0:
+				GPIO.output(led_R, 1) # 빨간색 LED 켬
+				GPIO.output(led_Y, 0) # 노란색 LED 끔
+				print("Motion Not Detected !")
+				debugNumber = -443
+				time.sleep(2)
+	except KeyboardInterrupt:
+		print("Stopped by User")
+		GPIO.cleanup() 
